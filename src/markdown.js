94c12810b995ea2c6ab591aceb29c06f6d7c8f9f
ZhangZisu <admin@zhangzisu.cn>
2018-09-28 22:29:18 +0800

    upload

--- src/markdown.js
+++ src/markdown.js
@@ -1,44 +1,22 @@
 import katex from "katex";
-import marked, { Renderer } from "marked";
+import zmark, { Renderer } from "zmark";
 import highlightjs from "highlight.js";
 import "katex/dist/katex.css";
-import mermaid from "mermaid";
-mermaid.initialize({});
 
-function highlightCode(code, language) {
+const renderer = new Renderer();
+renderer.code = (code, language) => {
   const validLang = !!(language && highlightjs.getLanguage(language));
   const highlighted = validLang
     ? highlightjs.highlight(language, code).value
     : code;
   return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`;
-}
-
-const renderer = new Renderer();
-renderer.code = (code, language) => {
-  switch (language) {
-    case "mermaid":
-      return `<pre class="mermaid">${code}</pre>`;
-    case "katex":
-      return katex.renderToString(code, { displayMode: true });
-    default:
-      return highlightCode(code, language);
-  }
 };
 
-renderer.codespan = text => {
-  if (text[0] === "$" && text[text.length - 1] === "$")
-    return katex.renderToString(text.substr(1, text.length - 2), {
-      displayMode: false
-    });
-  return "<code>" + text + "</code>";
-};
-
-marked.setOptions({ renderer: renderer, kaTex: katex });
+zmark.setOptions({ renderer: renderer, math: (text, display) => katex.renderToString(text, { displayMode: display }) });
 
 export default function render(markdown) {
   try {
-    let result = marked(markdown);
-    setTimeout(() => mermaid.init(), 50);
+    let result = zmark(markdown);
     return result;
   } catch (e) {
     return `<pre><code>${e.message}\nSource:\n${markdown}</code></pre>`;
