817c90f1c292d68e9240b6ea16a457478f1e607e
ZhangZisu <admin@zhangzisu.cn>
2018-10-07 20:27:04 +0800

    Change icon to perilla leave :happy:

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="UI inspired by Vuetify.js" class="mb-5">
+      <v-layout column align-center justify-center>
+        <img src="@/assets/logo.svg" alt="Perilla" class="mb-5" width="128px" height="128px"/>
         <div class="display-2" v-text="$t('home_welcome_title')"/>
         <div class="headline" v-text="$t('home_welcome_subtitle')"/>
       </v-layout>
